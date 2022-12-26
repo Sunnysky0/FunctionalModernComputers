9d23d2d642f332d612650ed4f432bdb3062beef5
Sunnysky0 <sunnysky1226@hotmail.com>
2022-12-26 01:31:43 -0500

    Changed to cmcl launcher

--- boost.py
+++ boost.py
@@ -1,5 +1,7 @@
-import shutil
 import os
+import shutil
 
-shutil.copy(".\\build\\libs\\FunctionalModernComputers-1.0-SNAPSHOT.jar", "D:\\HXY\\MDK\\.minecraft\\mods")
-os.system('.\\runClient.cmd')
+shutil.copy(".\\build\\libs\\FunctionalModernComputers-1.0-SNAPSHOT.jar", ".\\.minecraft\\mods")
+os.system("cp ./build/libs/FunctionalModernComputers-1.0-SNAPSHOT.jar "
+          "./cmcl/.minecraft/mods/FunctionalModernComputers-1.0-SNAPSHOT.jar")
+os.system('java -jar ./cmcl/CMCL.jar -start SMDK')
---
+++ initCMCL.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("java -jar cmcl/CMCL.jar -install 1.12.2 -n SMDK -o 14.23.5.2860")
